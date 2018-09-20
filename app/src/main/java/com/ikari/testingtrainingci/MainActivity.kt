1fce6df1de1279e3198d7d48b8fe3664c69dd062
ikarimeister <ik2233>
2018-09-20 16:06:15 +0200

    first code on UI

--- app/src/main/java/com/ikari/testingtrainingci/MainActivity.kt
+++ app/src/main/java/com/ikari/testingtrainingci/MainActivity.kt
@@ -2,11 +2,16 @@ package com.ikari.testingtrainingci
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loginBtn.setOnClickListener {
+            if(username.text.toString()=="admin"&&password.text.toString()=="admin")
+                loginBtn.text = "Log Out"
+        }
     }
 }
